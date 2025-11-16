ca56d2561e547b09f67fccb683b996935a497fb7
sofiane <sofianlkhassibi10@gmail.com>
2025-11-16 18:41:16 +0100

    refactor: restructure index.php and remove score.php; add new test.html

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="POST"></form>
+    <input type="number" name="fnum" palcholder="put ur first number here">
+        <input type="number" name="snum" palcholder="put ur second number here">
+        
+
+    
+</body>
+</html>
